42181bd1341a62a35172299303eb1affb8a13a44
Sil Trabalón <silvana.trabalon@gmail.com>
2021-05-04 22:47:31 -0300

    add script

---
+++ script.js
@@ -0,0 +1,272 @@
+//functions 
+function httpGet(theUrl){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); 
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+function cancel(){
+    background.style["display"] = 'block';
+    footer.style["display"] = 'block';
+    container.style['opacity'] = '0';
+    removed = document.querySelectorAll('.mycontent');
+    removed.forEach(function(e){
+        e.remove()
+    })
+}
+function createContent(){
+    content = document.createElement('tr');
+    content.classList.add('mycontent');
+    return content;
+}
+function addColum(value, header = false, state = false){
+    newColum = document.createElement('td');
+    if(header){
+        newColum.classList.add('h3');
+    }
+    newColum.classList.add('float-left', 'p-4', 'mycontent');
+
+    if (state){
+        if(value){
+            value.forEach(function(v){
+                tag = document.createElement('p');
+                tag.innerHTML = v;
+                newColum.appendChild(tag);
+            })
+        }else{
+            newColum.innerHTML = ' ';
+        }
+    }else{
+        newColum.innerHTML = value;
+    }
+    return newColum;
+}
+function createButtons(name){
+    btn = document.createElement('td');
+    btn.classList.add('btn', 'btn-sm','primary','flash-action');
+    btn.style['margin'] = '5px';
+    btn.classList.add(name);
+    btn.innerHTML = name;
+    header.appendChild(btn);
+}
+function renderHeaderTitle(){
+    content = createContent();
+    title = addColum('Title', true, false);
+    num = addColum('PR', true, false);
+    comment = addColum('Comments', true, false);
+    gannett = addColum('Gannett', true, false);
+    from = addColum('Reviewers', true, false);
+    opened = addColum('Opened', true, false);
+    statuss = addColum('Status', true, false);
+    content.appendChild(title);
+    content.appendChild(num);
+    content.appendChild(comment);
+    content.appendChild(gannett);
+    content.appendChild(from);
+    content.appendChild(opened);
+    content.appendChild(statuss);
+    container.appendChild(content);
+}
+function renderColumn(content, field, state = false){
+    element = addColum(field,false, state);
+    content.appendChild(element);
+    container.appendChild(content);
+}
+function download(tableID, filename = ''){
+    const dataType = 'application/vnd.ms-excel',
+        tableSelect = document.getElementById(tableID),
+        tableHTML = tableSelect.outerHTML.replace(/ /g, '%20'),
+        downloadLink = document.createElement("a");
+    filename = filename ? filename+'.xls' : 'metrics.xls';
+    document.body.appendChild(downloadLink);
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+        downloadLink.download = filename;
+        downloadLink.click();
+    }
+}
+
+//variables
+var   background = document.querySelector('.logged-in'),
+      footer = document.querySelector('.footer '),
+      container = document.querySelectorAll('[id = "js-flash-container"]'),
+      mycontainer = document.createElement('table'),
+      container = container[0].appendChild(mycontainer);
+container.style['width'] = '100%';
+container.style['backgroundColor'] = 'black';
+container.style['opacity'] = '0';
+
+header = document.createElement("tr");
+header.classList.add('flash', 'h1');
+header.innerHTML = 'Globant Team PRs';
+header.classList.add('mycontent');
+
+//renderHeaderTitle
+createButtons('download');
+createButtons('cancel');
+container.appendChild(header);
+renderHeaderTitle();
+
+array = [
+    {
+        "name": "Anto",
+        "user": "trabalonsilvana"
+    },
+    {
+       "name": "Carlos",
+        "user": "carlos-lopez-gannett"
+    },
+    {
+        "name": "Claus",
+        "user": "claushessegannett"
+    },
+    {
+        "name": "Jose",
+        "user": "josedimarco"
+    },
+    {
+        "name": "Jp",
+        "user": "juandelcastillo"
+    },
+    {
+        "name": "Leo",
+        "user": "orsileonel-gannett"
+    },
+    {
+        "name": "Nacho",
+        "user": "jose-decima"
+    },
+    {
+        "name": "Sil",
+        "user": "sgarciagannett"
+    },
+    {
+        "name": "Kevin",
+        "user": "KevinTriana-Gannett"
+    },
+    {
+        "name": "Francisco",
+        "user": "frxncismor "
+    }
+];
+function checkArray(rev,array){
+    isGlb = false;
+    array.forEach(function(author){
+        if(author.user === rev || 'Rodrigo-gannett' === rev){
+            isGlb = true;
+        }
+    })
+    return isGlb;
+}
+
+ function render (user,name){
+    var mysite = httpGet("https://github.com/GannettDigital/tangent/pulls/" + user),   
+        doc = document.createElement( 'html' );
+doc.innerHTML = mysite;
+const prs = doc.querySelectorAll('[id ^= "issue_"]'),
+      activePr = [];
+      flag = false;
+    if(prs){
+        prs.forEach(async function(pr) {
+            let title, elementLabels, tags = [], labels, label = [], datetime, opened;
+            if (pr.classList.contains('Box-row')) {
+                flag = true;
+                // pr.classList.add("mystyle");
+                title = pr.querySelector('a[data-hovercard-type="pull_request"]').text;
+                elementLabels = pr.querySelectorAll('.labels');
+                elementLabels.forEach(function(lbs){
+                    labels = lbs.querySelectorAll('.IssueLabel');
+                    labels.forEach(function(lb){
+                        label.push(lb.text); 
+                    });
+                    tags.push(label);
+                })
+                datetime = pr.querySelectorAll("relative-time[datetime]");
+                opened = datetime[0].title;
+                d = new Date (opened);
+                if (isNaN(d)){
+                    d = opened.split(' GMT-3')[0]
+                }else{
+                    d = d.toLocaleString().split(' ')[0]
+                }
+                comments = pr.querySelectorAll("span.text-small.text-bold");
+                if (comments[comments.length -1]){
+                    noc = comments[comments.length -1].innerText;
+                    noc = noc.trim();
+                    if (isNaN(parseInt(noc))) {
+                        noc = '0';
+                    }
+                }else{
+                    noc = '0'
+                }
+
+                num = pr.id.split('issue_')[1];
+                var mypr = httpGet("https://github.com/GannettDigital/tangent/pull/" + num);
+                docum = document.createElement( 'html' );
+                docum.innerHTML = mypr;
+
+                timeline = docum.querySelectorAll('.TimelineItem-body.d-flex');
+                reviewers = [];
+                reviews = [];
+
+                if(timeline.length > 0){
+                    timeline.forEach(function(review){
+                        if(review.outerText.includes('reviewed')){
+                            rev = review.getElementsByTagName("strong")[0].innerText.trim();
+                            check = checkArray(rev,array);
+                            if (!check && rev != 'New changes since you last viewed'){
+                                reviews.push(rev);
+                            }
+                        }
+                    })
+                }
+
+                numRev = reviews.length;
+                if (numRev > 0){
+                    reviewers = reviews.filter((el, index) => reviews.indexOf(el) === index);
+                }else{
+                    reviewers = '';
+                }
+                activePr.push({'title': title.split(' ')[0], 'labels': tags[0], 'opened' : d, 'comments': noc.trim(), 'pr': num, 'gannett': numRev, 'from': reviewers });
+            }
+        })
+    }
+    content = createContent();
+    author = addColum(name, true , false);
+    content.appendChild(author);
+    container.appendChild(content);
+
+    //Render grid
+    activePr.forEach(function(e){
+        content = createContent();
+        renderColumn(content, e.title);
+        renderColumn(content, e.pr);
+        renderColumn(content, e.comments);
+        renderColumn(content, e.gannett);
+        renderColumn(content, e.from);
+        renderColumn(content, e.opened);
+        renderColumn(content, e.labels , true);
+    })
+}
+
+array.forEach(function(author){
+    render(author.user, author.name);
+})
+
+cancelButton = document.querySelector('.cancel');
+cancelButton.onclick = function() {cancel()};
+downloadButton = document.querySelector('.download');
+downloadButton.onclick = function() {
+    download('js-flash-container')
+};
+
+if (flag) {
+    footer.style["display"] = 'none';
+    container.style['opacity'] = '1';
+}
+
